4a45021d678b0f85eed12f1957af0d0e39c331c8
Avalon <210815572+Avalon96@users.noreply.github.com>
2026-07-01 01:18:12 +0300

    added !del added reaction to message added role requirements

--- main.py
+++ main.py
@@ -16,11 +16,31 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 custom_commands = {}
+ALLOWED_ROLES = ("Uma Musume Vice Pope", "The Island Owner")
 
 @bot.command()
 async def command(ctx, name: str, *, response: str):
+
+    if not any(role.name in ALLOWED_ROLES for role in ctx.author.roles):
+        sent_message = await ctx.send("Only Uma Musume Vice Popes and The Island Owner can use this command.")
+        await sent_message.add_reaction("<:LucyPat:1521635572907774072>")
+        return
+
     custom_commands[name] = response
     await ctx.send(f"Saved command `{name}` → `{response}`")
+
+@bot.command(name="del")
+async def delete_command(ctx, name: str):
+    if not any(role.name in ALLOWED_ROLES for role in ctx.author.roles):
+        sent_message = await ctx.send("Only Uma Musume Vice Popes and The Island Owner can use this command.")
+        await sent_message.add_reaction("<:LucyPat:1521635572907774072>")
+        return
+
+    if name in custom_commands:
+        del custom_commands[name]
+        await ctx.send(f"Removed command `{name}`")
+    else:
+        await ctx.send(f"Command `{name}` not found")
 
 @bot.event
 async def on_message(message):
@@ -29,12 +49,11 @@ async def on_message(message):
 
     content = message.content
 
-    # If message matches stored command
     if content in custom_commands:
-        await message.channel.send(custom_commands[content])
+        sent_message = await message.channel.send(custom_commands[content])
+        await sent_message.add_reaction("<a:LucyPat:1521201578474737754>")
         return
 
-    # Still allow normal commands like !command to work
     await bot.process_commands(message)
 
 
